a313cf9ff5bc8ec32f974dbd37a278c4af59d9ae
Natasha Johnson <njohnson533@gmail.com>
2024-07-22 23:48:18 -0400

    chore: add social media component

---
+++ src/app/(site)/components/SocialMediaLinks.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { FaLinkedin, FaInstagram, FaTwitter, FaFacebook } from "react-icons/fa";
+
+type Props = {
+  socialMedia: {
+    linkedIn: string;
+    instagram: string;
+    twitter: string;
+    facebook: string;
+  };
+};
+
+const SocialMediaLinks = ({ socialMedia }: Props) => {
+  const { linkedIn, facebook, instagram, twitter } = socialMedia;
+  return (
+    <div className="flex justify-between text-2xl w-48 text-accent border">
+      {facebook && (
+        <FaFacebook
+          className={`${!linkedIn && !instagram && !twitter ? "ml-auto" : ""}`}
+        />
+      )}
+      {linkedIn && (
+        <FaLinkedin
+          className={`${!facebook && !instagram && !twitter ? "ml-auto" : ""}`}
+        />
+      )}
+      {instagram && (
+        <FaInstagram
+          className={`${!facebook && !linkedIn && !twitter ? "ml-auto" : ""}`}
+        />
+      )}
+      {twitter && (
+        <FaTwitter
+          className={`${!facebook && !linkedIn && !instagram ? "ml-auto" : ""}`}
+        />
+      )}
+    </div>
+  );
+};
+
+export default SocialMediaLinks;

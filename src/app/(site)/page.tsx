f93d8761529601c627186063deb07a8f3c91fa04
Natasha Johnson <njohnson533@gmail.com>
2024-07-26 01:20:35 -0400

    chore: add google map to home page

--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -14,8 +14,6 @@ const Home = async () => {
     return <div className="mt-32">Page not found</div>;
   }
 
-  // console.log("pagedata ----->", pageData.content);
-
   return (
     <>
   
@@ -36,6 +34,7 @@ const Home = async () => {
             return null;
         }
       })}
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3164.5089665145665!2d-77.61718592348326!3d37.51949787205098!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x89b16da3bdf04f59%3A0x1222ac159601833e!2sAtlas%20Behavioral%20Health%20and%20Wellness!5e0!3m2!1sen!2sus!4v1721970757728!5m2!1sen!2sus" width="600" height="350" className="border-none w-full" allowFullScreen loading="lazy" referrerPolicy="no-referrer-when-downgrade"></iframe>
     </>
   );
 };

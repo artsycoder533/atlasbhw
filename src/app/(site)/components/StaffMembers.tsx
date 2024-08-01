cb9b36a6266d13ff84370ebcc82ad21e215754c7
Natasha Johnson <njohnson533@gmail.com>
2024-08-01 18:29:26 -0400

    chore: make staff section responsive

--- src/app/(site)/components/StaffMembers.tsx
+++ src/app/(site)/components/StaffMembers.tsx
@@ -11,12 +11,12 @@ const StaffMembers = ({data}: Props) => {
     const {title, staffMembers} = data;
     console.log('data ====>', data)
   return (
-    <section className="max-w-7xl w-[90vw] mx-auto py-12" id="staff">
+    <section className="max-w-7xl w-[90vw] mx-auto py-12 mb-12" id="staff">
         <div className="flex justify-center mb-6">
         <Title title={title} size="lg"/>
         </div>
         
-        <div className="flex gap-10">
+        <div className="flex flex-col xl:flex-row gap-10">
             {staffMembers.map(staffMember => <StaffMember key={staffMember._id} staff={staffMember} />)}
         </div>
     </section>

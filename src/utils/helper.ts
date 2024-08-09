ac1249346619de839b485de317cc479b487ee62e
Natasha Johnson <njohnson533@gmail.com>
2024-08-09 02:29:25 -0400

    chore: remove unused import

--- src/utils/helper.ts
+++ src/utils/helper.ts
@@ -1,7 +1,5 @@
 import imageUrlBuilder from "@sanity/image-url";
 import { projectId, dataset } from "../../sanity/env";
-import { getPagesByDocumentTypeQuery } from "../../sanity/lib/queries";
-import { sanityFetch } from "../../sanity/lib/fetch";
 
 export const getYear = () => {
     const date = new Date();
